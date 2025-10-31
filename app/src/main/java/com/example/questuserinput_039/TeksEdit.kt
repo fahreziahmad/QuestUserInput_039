f435e70913fe1837c4add84d8354dab27cec385c
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-10-31 16:46:26 +0700

    menambahkan komponen kartu (ElevatedCard) untuk menampilkan konten dengan efek elevasi (bayangan), menggunakan warna latar hitam dan ukuran 300×100dp.

--- app/src/main/java/com/example/questuserinput_039/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_039/TeksEdit.kt
@@ -114,3 +114,10 @@ fun FormDataDiri(
             color = Color.DKGRAY
         )
 
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.BLACK),
+            modifier = Modifier
+                .height(height = 100.dp)
+                .width(width = 300.dp)
+        ) {
