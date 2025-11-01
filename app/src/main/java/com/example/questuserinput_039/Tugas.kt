083692f5e1bc44ef93e0dc903580f7792fd6c3c8
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-01 20:49:11 +0700

    menambahkan komponen pemilihan tanggal (DatePickerDialog) agar pengguna dapat memilih tanggal lahir secara interaktif, bukan mengetik manua

--- app/src/main/java/com/example/questuserinput_039/Tugas.kt
+++ app/src/main/java/com/example/questuserinput_039/Tugas.kt
@@ -33,3 +33,16 @@ fun FormRegistrasi() {
     var disetujui by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
 
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _, year, month, dayOfMonth ->
+            tanggalLahir = "$dayOfMonth/${month + 1}/$year"
+        },
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH),
+        calendar.get(Calendar.DAY_OF_MONTH)
+    )
+
